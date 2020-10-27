75171f63d92e24f73b97c50cb19d7ff8b16bf9a9
Haseeb Khan <muhammadhaseebkhan96@gmail.com>
2020-10-27 20:01:30 +0500

    formating

--- src/assets/Components/Pages/DashboadSection/SectionAcountFilter.js
+++ src/assets/Components/Pages/DashboadSection/SectionAcountFilter.js
@@ -7,166 +7,162 @@ function SectionAcountFilter(props) {
     //const { t, i18n } = this.props
     return (
 
-        <
-        section class = "search-filter wow fadeInUp"
-        data - wow - delay = "0.6s" >
-        <
-        div class = "container" >
-        <
-        div class = "row" >
-        <
-        div class = "col-md-12" >
-        <
-        ul class = "nav nav-pills nav-fill" >
-        <
-        li class = "nav-item" >
-        <
-        Link to = "/lolaccount" >
+        <section class="search-filter wow fadeInUp" data - wow - delay = "0.6s" >
+        <div class="container" >
+            <div class="row" >
+                <
+        div class="col-md-12" >
+                    <
+        ul class="nav nav-pills nav-fill" >
+                        <
+        li class="nav-item" >
+                            <
+        Link to="/lolaccount" >
 
-        <
-        a class = "nav-link active"
-        href = "" > LOL ACCOUNTS < /a> <
+                                <
+        a class="nav-link active"
+                                    href="" > LOL ACCOUNTS < /a> <
         /Link> <
         /li> <
-        li class = "nav-item" >
-        <
-        Link to = "/unrankedaccount" >
+        li class="nav-item" >
+                                        <
+        Link to="/unrankedaccount" >
 
-        <
-        a class = "nav-link"
-        href = "" > UNRANKED ACCOUNTS < /a> <
+                                            <
+        a class="nav-link"
+                                                href="" > UNRANKED ACCOUNTS < /a> <
         /Link> <
         /li> <
-        li class = "nav-item" >
-        <
-        Link to = "/customizedaccount" >
-        <
-        a class = "nav-link"
-        href = "" > CUSTOMIZED ACCOUNTS < /a> <
+        li class="nav-item" >
+                                                    <
+        Link to="/customizedaccount" >
+                                                        <
+        a class="nav-link"
+                                                            href="" > CUSTOMIZED ACCOUNTS < /a> <
         /Link> <
         /li> <
         /ul>
 
         <
-        div class = "card" >
-        <
-        div class = "card-body" >
-        <
+        div class="card" >
+                                                                <
+        div class="card-body" >
+                                                                    <
         form >
-        <
-        div class = "form-row" >
-        <
-        div class = "form-group col-sm-6 col-lg-2" >
-        <
+                                                                        <
+        div class="form-row" >
+                                                                            <
+        div class="form-group col-sm-6 col-lg-2" >
+                                                                                <
         label > SERVER < /label> <
-        select class = "form-control" >
-        <
+        select class="form-control" >
+                                                                                        <
         option > All < /option> <
         option > 1 < /option> <
         option > 2 < /option> <
         /select> <
         /div> <
-        div class = "form-group col-sm-6 col-lg-2" >
-        <
+        div class="form-group col-sm-6 col-lg-2" >
+                                                                                                        <
         label > TIERS < /label> <
-        select class = "form-control" >
-        <
+        select class="form-control" >
+                                                                                                                <
         option > All < /option> <
         option > 1 < /option> <
         option > 2 < /option> <
         /select> <
         /div> <
-        div class = "form-group col-sm-6 col-lg-2" >
-        <
+        div class="form-group col-sm-6 col-lg-2" >
+                                                                                                                                <
         label > SPECIAL INFORMATIONS < /label> <
-        select class = "form-control" >
-        <
+        select class="form-control" >
+                                                                                                                                        <
         option > None < /option> <
         option > 1 < /option> <
         option > 2 < /option> <
         /select> <
         /div> <
-        div class = "form-group col-sm-6 col-lg-2" >
-        <
+        div class="form-group col-sm-6 col-lg-2" >
+                                                                                                                                                        <
         label > EXPERT PICK < /label> <
-        select class = "form-control" >
-        <
+        select class="form-control" >
+                                                                                                                                                                <
         option > All < /option> <
         option > 1 < /option> <
         option > 2 < /option> <
         /select> <
         /div> <
-        div class = "form-group col-sm-6 col-lg-2" >
-        <
+        div class="form-group col-sm-6 col-lg-2" >
+                                                                                                                                                                                <
         label > HANDMADE / BOTTED < /label> <
-        select class = "form-control" >
-        <
+        select class="form-control" >
+                                                                                                                                                                                        <
         option > None < /option> <
         option > 1 < /option> <
         option > 2 < /option> <
         /select> <
         /div> <
-        div class = "form-group col-sm-6 col-lg-2" >
-        <
+        div class="form-group col-sm-6 col-lg-2" >
+                                                                                                                                                                                                        <
         label > DISCOUNTED < /label> <
-        select class = "form-control" >
-        <
+        select class="form-control" >
+                                                                                                                                                                                                                <
         option > All < /option> <
         option > 1 < /option> <
         option > 2 < /option> <
         /select> <
         /div> <
-        div class = "form-group col-sm-6 col-lg-2" >
-        <
+        div class="form-group col-sm-6 col-lg-2" >
+                                                                                                                                                                                                                                <
         label > LOL ACCOUNTS < /label> <
-        select class = "form-control" >
-        <
+        select class="form-control" >
+                                                                                                                                                                                                                                        <
         option > All < /option> <
         option > 1 < /option> <
         option > 2 < /option> <
         /select> <
         /div> <
-        div class = "form-group col-sm-6 col-lg-2" >
-        <
+        div class="form-group col-sm-6 col-lg-2" >
+                                                                                                                                                                                                                                                        <
         label > TYPE OF QUEUE < /label> <
-        select class = "form-control" >
-        <
+        select class="form-control" >
+                                                                                                                                                                                                                                                                <
         option > None < /option> <
         option > 1 < /option> <
         option > 2 < /option> <
         /select> <
         /div> <
-        div class = "col-md-4" >
-        <
-        div class = "slider-box" >
-        <
+        div class="col-md-4" >
+                                                                                                                                                                                                                                                                                <
+        div class="slider-box" >
+                                                                                                                                                                                                                                                                                    <
         label
-        for = "priceRange" > Price Range: < /label> <
-        div id = "price-range"
-        class = "slider ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all" >
-        <
-        div class = "ui-slider-range ui-widget-header ui-corner-all"
-        style = {
-            { width: "100%", left: "0%" } } >
-        <
+                                                                                                                                                                                                                                                                                        for="priceRange" > Price Range: < /label> <
+        div id="price-range"
+                                                                                                                                                                                                                                                                                            class="slider ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all" >
+                                                                                                                                                                                                                                                                                            <
+        div class="ui-slider-range ui-widget-header ui-corner-all"
+                                                                                                                                                                                                                                                                                                style={
+                                                                                                                                                                                                                                                                                                    { width: "100%", left: "0%" }} >
+                                                                                                                                                                                                                                                                                                <
         /div> <
-        span class = "ui-slider-handle ui-state-default ui-corner-all"
-        tabindex = "0"
-        style = {
-            { left: "0%" } } >
-        <
+        span class="ui-slider-handle ui-state-default ui-corner-all"
+                                                                                                                                                                                                                                                                                                    tabindex="0"
+                                                                                                                                                                                                                                                                                                    style={
+                                                                                                                                                                                                                                                                                                        { left: "0%" }} >
+                                                                                                                                                                                                                                                                                                    <
         /span> <
-        span class = "ui-slider-handle ui-state-default ui-corner-all"
-        tabindex = "0"
-        style = {
-            { left: "0%" } } >
-        <
+        span class="ui-slider-handle ui-state-default ui-corner-all"
+                                                                                                                                                                                                                                                                                                        tabindex="0"
+                                                                                                                                                                                                                                                                                                        style={
+                                                                                                                                                                                                                                                                                                            { left: "0%" }} >
+                                                                                                                                                                                                                                                                                                        <
         /span> <
         /div> <
-        input type = "text"
-        id = "priceRange"
-        readonly / >
-        <
+                                                                                                                                                                                                                                                                                                            input type="text"
+                                                                                                                                                                                                                                                                                                            id="priceRange"
+                                                                                                                                                                                                                                                                                                            readonly />
+                                                                                                                                                                                                                                                                                                        <
         /div>
 
         <
